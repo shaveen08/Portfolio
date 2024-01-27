e31ea428f7356f6f7158e9bd906fce27f1ae757e
Shaveen <155416600+shaveen08@users.noreply.github.com>
2024-01-27 18:13:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+//My resume Download btn
+document.querySelector('#cv').addEventListener('click', ()=>{
+    var link = document.createElement('a');
+    link.href = './SHAVEEN_resume.pdf'; 
+    link.download = 'SHAVEEN_resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+//Contact info
+document.getElementById('contact-info').addEventListener('click', function() {
+    var contactSection = document.getElementById('contact');
+    if (contactSection.style.display === 'none' || contactSection.style.display === '') {
+        contactSection.style.display = 'block';
+        contactSection.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        contactSection.style.display = 'none';
+    }
+});
+
+//LinkedIn Profile url btn
+document.getElementById('linkdin').addEventListener('click', ()=>{
+    window.location.href = "https://www.linkedin.com/in/shaveen-kumar08";
+});
+
+//Github Profile url btn
+document.getElementById('github').addEventListener('click', ()=>{
+    window.location.href = "https://github.com/shaveen08";
+});
+
+//Rock Paper Scissor Game btn
+document.getElementById('rps-game-link').addEventListener('click', ()=>{
+    window.location.href = "https://github.com/shaveen08/RockPaperScissor-Game";
+});
+
+//E-commerce website btn
+document.getElementById('ecommerce-link').addEventListener('click', ()=>{
+    window.location.href = "https://github.com/shaveen08/E-commerce-website";
+});
+
+//E-commerce website btn
+document.getElementById('netflix-link').addEventListener('click', ()=>{
+    window.location.href = "https://github.com/shaveen08/Netflix-clone";
+});
+
+//Basic project btn
+document.getElementById('basic-proj-link').addEventListener('click', ()=>{
+    window.location.href = "https://github.com/shaveen08/Basic-Projects";
+});
